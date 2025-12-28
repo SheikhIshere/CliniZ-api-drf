3c944d47d50784bcfe2998032fa9ea9c87237592
SheikhIshere <thesheikh255@gmail.com>
2025-12-29 01:31:26 +0600

    feat: post gress database added

--- App/core/settings.py
+++ App/core/settings.py
@@ -35,8 +35,9 @@ SECRET_KEY = 'django-insecure-====3=lu2x*7sxn)qpvs4^mtc$##@k$=2^))g==hyrz_(sbccg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cliniz-api-drf.onrender.com']
+ALLOWED_HOSTS = ['cliniz-api-drf.onrender.com', 'localhost', '127.0.0.1']
 
+# 
 
 # Application definition
 
@@ -121,10 +122,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
